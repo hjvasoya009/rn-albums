2782a3a3b3d65abcf0b20edccb8ea641febdc20d
H J Vasoya <65635239+hjvasoya009@users.noreply.github.com>
2021-01-17 09:10:52 -0800

    App Wrap up with Touchable Opacity and Linking

--- index.js
+++ index.js
@@ -10,12 +10,12 @@
 
 import React from 'react';
 import { AppRegistry, View } from 'react-native';
-import Header from './src/components/HeaderC';
+import Header from './src/components/Header';
 import AlbumList from './src/components/AlbumList';
 
 const App = () => {
     return (
-        <View>
+        <View style={{ flex: 1 }}>
             <Header headerText={'Albums'} />
             <AlbumList />
         </View>
--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,23 +1,30 @@
 import React from 'react';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, StyleSheet, Linking } from 'react-native';
+import Button from './Button';
 import Card from './Card';
 import CardSection from './CardSection';
 
 const AlbumDetail = ({ album }) => {
-    const { title, artist, thumbnail_image } = album;
-    const { thumbnailStyle, headerContentStyle } = styles;
+    const { title, artist, thumbnail_image, image, url } = album;
+    const { 
+        thumbnailStyle,
+        headerContentStyle,
+        thumbnailContainerStyle,
+        headerTextStyle,
+        imageStle
+    } = styles;
 
     return (
         <Card>
             <CardSection>
-                <View>
+                <View style={thumbnailContainerStyle}>
                     <Image
                         style={thumbnailStyle}
                         source={{ uri: thumbnail_image }}
                     />
                 </View>
                 <View style={headerContentStyle}>
-                    <Text>
+                    <Text style={headerTextStyle}>
                         {title}
                     </Text>
                     <Text>
@@ -25,19 +32,46 @@ const AlbumDetail = ({ album }) => {
                     </Text>
                 </View>
             </CardSection>
+            <CardSection>
+                <Image
+                    style={imageStle}
+                    source={{ uri: image }}
+                />
+            </CardSection>
+            <CardSection>
+                <Button
+                    onPress={() => Linking.openURL(url)}
+                >
+                    Buy Now
+                </Button>
+            </CardSection>
         </Card>
     );
 };
 
-const styles = {
+const styles = StyleSheet.create({
     headerContentStyle: {
         flexDirection: 'column',
         justifyContent: 'space-around'
     },
+    headerTextStyle: {
+        fontSize: 18
+    },
     thumbnailStyle: {
         height: 50,
         width: 50,
+    },
+    thumbnailContainerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginRight: 10
+    },
+    imageStle: {
+        height: 300,
+        flex: 1,
+        width: null
     }
-};
+});
 
 export default AlbumDetail;
